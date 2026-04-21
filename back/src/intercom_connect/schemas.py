0b16eaf1e9a1d5c47083fc82139f95750f9057c1
unknown <andreyka.drogan@gmail.com>
2026-04-21 16:29:23 +0500

    y

--- back/src/intercom_connect/schemas.py
+++ back/src/intercom_connect/schemas.py
@@ -1,28 +1,38 @@
 
-from typing import Optional
+from typing import List, Optional
 from fastapi import WebSocket
 from pydantic import BaseModel
 from typing import Optional, Union, Any, Dict
 
 class BlockDevice(BaseModel):
-    id: int
-    name: str
-    owner_id: str
-    addr: str
-    status: int
-    is_owner: bool
-    id_permission: Optional[Union[int, str]]  # может быть int или str
-    is_one_access: bool
-    rules: list
-    type: str
-    photo_url: Optional[str]
-    owner_short_name: str
-    is_time_access: bool
-    has_access: Optional[Union[bool, str]]  # может быть bool или str
-    time_access: Optional[Union[Dict, str]]  # может быть dict или str
-    geo_lat: float
-    geo_lon: float
-    kladr_id: str
+    model_config = {
+        "extra": "allow"  # игнорировать лишние поля
+    }
+    id: Optional[int] = None
+    name: Optional[str] = None
+    owner_id: Optional[str] = None
+    addr: Optional[str] = None
+    status: Optional[int] = None
+    is_owner: Optional[bool] = None
+
+    id_permission: Optional[Union[int, str]] = None
+    is_one_access: Optional[bool] = None
+
+    rules: Optional[List[Any]] = None
+    type: Optional[str] = None
+
+    photo_url: Optional[str] = None
+    owner_short_name: Optional[str] = None
+
+    is_time_access: Optional[bool] = None
+    has_access: Optional[Union[bool, str]] = None
+
+    time_access: Optional[Union[Dict[str, Any], str]] = None
+
+    geo_lat: Optional[float] = None
+    geo_lon: Optional[float] = None
+
+    kladr_id: Optional[str] = None
 
 class BaseCallData(BaseModel):
     house_id: Optional[int] = None
